dc2f94358ce14821a24d36950de2bae46e6c6e9b
qmzsky <775832781@qq.com>
2022-10-11 13:11:41 +0800

    v8

---
+++ configs/cao/jsxs_cao_1x.py
@@ -0,0 +1,25 @@
+_base_ = '../cao/cao_roi_faster.py'
+
+# 修改数据集相关设置
+dataset_type = 'CocoDataset'
+classes = ('jsxs',)
+data = dict(
+    train=dict(
+        img_prefix='/data/cy/jsxs/train2017/',
+        classes=classes,
+        ann_file='/data/cy/jsxs/annotations/instances_train2017.json'),
+    val=dict(
+        img_prefix='/data/cy/jsxs/val2017/',
+        classes=classes,
+        ann_file='/data/cy/jsxs/annotations/instances_val2017.json'),
+    test=dict(
+        img_prefix='/data/cy/jsxs/val2017/',
+        classes=classes,
+        ann_file='/data/cy/jsxs/annotations/instances_val2017.json'))
+
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=1))
+    )
+
+load_from = 'checkpoints/cao_roi_faster.pth.pth'
