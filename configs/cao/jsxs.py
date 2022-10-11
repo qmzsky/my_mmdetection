7db16e3a3534ea8a2e372c985a061614879955a8
qmzsky <775832781@qq.com>
2022-10-11 10:31:28 +0800

    v8

---
+++ configs/cao/jsxs.py
@@ -0,0 +1,26 @@
+_base_ = '../faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
+
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=1),
+        mask_head=dict(num_classes=1)))
+
+# 修改数据集相关设置
+dataset_type = 'CocoDataset'
+classes = ('jsxs',)
+data = dict(
+    train=dict(
+        img_prefix='./data/cy/jsxs/train2017/',
+        classes=classes,
+        ann_file='./data/cy/jsxs/annotations/instances_train2017.json'),
+    val=dict(
+        img_prefix='./data/cy/jsxs/val2017/',
+        classes=classes,
+        ann_file='./data/cy/jsxs/annotations/instances_train2017.json'),
+    test=dict(
+        img_prefix='./data/cy/jsxs/val2017/',
+        classes=classes,
+        ann_file='./data/cy/jsxs/annotations/instances_train2017.json'))
+
+load_from = './checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+
