7c7cb848b071f9377747d352bac3daa9d6515136
qmzsky <775832781@qq.com>
2022-10-11 10:46:41 +0800

    v8

--- configs/cao/jsxs.py
+++ configs/cao/jsxs.py
@@ -16,11 +16,11 @@ data = dict(
     val=dict(
         img_prefix='/data/cy/jsxs/val2017/',
         classes=classes,
-        ann_file='/data/cy/jsxs/annotations/instances_train2017.json'),
+        ann_file='/data/cy/jsxs/annotations/instances_val2017.json'),
     test=dict(
         img_prefix='/data/cy/jsxs/val2017/',
         classes=classes,
-        ann_file='/data/cy/jsxs/annotations/instances_train2017.json'))
+        ann_file='/data/cy/jsxs/annotations/instances_val2017.json'))
 
 load_from = 'checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
 
